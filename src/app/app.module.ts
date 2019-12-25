fa2e404e823e099c1530222397bd4b83adc07bc3
Jonny <jonathan44472@gmail.com>
2019-12-25 23:25:07 +0000

    heading component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { HeadingComponent } from './heading/heading.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeadingComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
