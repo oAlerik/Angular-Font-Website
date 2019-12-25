226fb08265f4fe60ef09f437f176b2f662f56448
Jonny <jonathan44472@gmail.com>
2019-12-25 23:40:13 +0000

    text-field component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { HeadingComponent } from './heading/heading.component';
+import { TextFieldComponent } from './text-field/text-field.component';
 
 @NgModule({
-  declarations: [AppComponent, HeadingComponent],
+  declarations: [AppComponent, HeadingComponent, TextFieldComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
